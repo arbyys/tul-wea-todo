bc24265fe3da607c0c87c5def22dd1d14ed9fd31
arbyys <adam.petricek@centrum.cz>
2023-11-30 18:03:46 +0100

    Initial structure

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+def home():
+    return render_template('pages/home.html')
