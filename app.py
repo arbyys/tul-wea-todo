84e8ab918e36e82ee583ee0800fcb3b8900e9d53
arbyys <adam.petricek@centrum.cz>
2023-12-06 18:50:50 +0100

    Docker settings

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('pages/home.html')
+
+
+if __name__ == '__main__':
+    host = os.getenv("APP_HOST")
+    port = os.getenv("APP_PORT")
+    debug = os.getenv("APP_DEBUG")
+
+    app.run(host=host, port=port, debug=debug)
